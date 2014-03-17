47d77f6cb2d9aabfbb338eba97fb91732fde7166
karkas1 <karolis.kasys@ktu.edu>
2014-03-17 21:39:45 +0200

    added authentication and data contracts which arent used (yet)

---
+++ twitter/ConsoleTest/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Business;
+
+namespace ConsoleTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Authentication stuff = new Authentication();
+            int status = stuff.Authenticate();
+        }
+    }
+}
